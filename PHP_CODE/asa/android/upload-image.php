90454b4e53b9002dc655c78b5cc206c1933e91a6
Aprijal Pasaribu <aprijal.psrb@gmail.com>
2015-04-11 20:42:44 +0700

    PHP_CODE

---
+++ PHP_CODE/asa/android/upload-image.php
@@ -0,0 +1,10 @@
+<?php
+$base = $_REQUEST['image'];
+$filename = $_REQUEST['filename'];
+$binary = base64_decode($base);
+header('Content-Type: bitmap; charset=utf-8');
+$file = fopen('uploadedimages/'.$filename, 'wb');
+fwrite($file, $binary);
+fclose($file);
+echo 'Image upload complete, Please check your php file directory';
+?>
